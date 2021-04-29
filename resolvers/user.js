d94e7b802e12cdb2aa12bb82fd941dbe7c70d915
Sohail khan <sohailhumdard@gmail.com>
2021-04-29 18:11:10 +0500

    connect with mongo db

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,5 +1,6 @@
 
 const { users, tasks } = require('../constaints');
+const User = require('../database/models/user');
 module.exports = {
 
     Query: {
@@ -7,6 +8,17 @@ module.exports = {
         user: (_, { id }) => users.find(user => user.id ===  id)
     },
     Mutation: {
+        signup: async (_ , { input }) => {
+            try {
+                const user = await User.findOne({ email: input.email });
+                if (user){
+                    throw new Error('email already in use')
+                }
+            } catch {
+                console.log(error);
+                throw error;
+            }
+        }
     },
     User: {
         tasks: ({ id }) => tasks.filter(task => task.userId === id)
